8c49519014cba07c733f7889653ea9790b6b65f7
Piotr Tajanowicz <piotr.tajanowicz@gmail.com>
2024-12-31 22:07:33 -0600

    CORS setting

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-// app.use(cors());
+app.use(cors());
 // Enable CORS for all domains or specific domains
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+// }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
